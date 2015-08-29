ff088c02d7b53ef491efcd6fe45e3a26a0e26db8
FatimaE <perezrodriguezfe@gmail.com>
2015-08-29 17:09:56 -0600

    crup proyecto

---
+++ src/main/java/com/siacra/daos/ProyectoDao.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siacra.daos;
+
+import com.siacra.models.Proyecto;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author SIACRA Devlopment Team
+ * @since 19/08/2015
+ * @version 1.0.0
+ */
+@Repository
+public class ProyectoDao {
+    
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    
+    public void addProyecto(Proyecto proyecto){
+        getSessionFactory().getCurrentSession().save(proyecto);
+    }
+    
+    
+    public void deleteProyecto(Proyecto proyecto){
+        getSessionFactory().getCurrentSession().delete(proyecto);
+    }
+    
+    
+    public void updateProyecto(Proyecto proyecto){
+        getSessionFactory().getCurrentSession().update(proyecto);
+    } 
+    
+    
+    public Proyecto getProyectoById(int id){
+        List list = getSessionFactory().getCurrentSession()
+                .createQuery("from Proyecto where idproyecto=?").setParameter(0, id).list();
+        return (Proyecto)list.get(0); 
+    }
+    
+    
+    public List<Proyecto> getProyectos(){
+        List list = getSessionFactory().getCurrentSession().createQuery("from Proyecto").list();
+        return list; 
+    }
+    
+    
+}
