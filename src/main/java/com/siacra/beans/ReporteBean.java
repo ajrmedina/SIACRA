3b7e488630423493dd2c112ceb191b526664f975
ajrmedina <ajr1201@gmail.com>
2015-09-19 23:37:11 -0600

    Reportes TG y Proyectos

--- src/main/java/com/siacra/beans/ReporteBean.java
+++ src/main/java/com/siacra/beans/ReporteBean.java
@@ -6,10 +6,12 @@
 package com.siacra.beans;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -22,6 +24,7 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import org.primefaces.context.RequestContext;
 
 
 /**
@@ -30,25 +33,48 @@ import net.sf.jasperreports.engine.JasperPrint;
  */
 @ManagedBean(name="reporte")
 @ViewScoped
-public class ReporteBean {
+public class ReporteBean implements Serializable{
     
     private String codigoEscuela;
-   
+    private String filterAnio;
+    private String filterEstadoTGP;
+    private String nombreReporte;
+    private boolean disabled=true;
+    
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/siacra", "root", "123");
         return conexion;
     }
     
-    public void exportarServicioP(String nombre) throws JRException, ClassNotFoundException, SQLException, IOException{
+    public void exportarServicioP() throws JRException, ClassNotFoundException, SQLException, IOException{
+        //Obtener el contexto del servlet
         ServletContext context = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();  
-        String ubicacion = "/WEB-INF/web-reports/"+nombre+".jasper";
-        String reporte = context.getRealPath(ubicacion);
+        //Obtener la lista de parametros provenientes de la vista
+        Map<String,String> viewparams = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        //Ubicacion del reporte
+        String reporte = context.getRealPath("/WEB-INF/web-reports/"+getNombreReporte()+".jasper");
+        //Parametros para el reporte
         HashMap parameter = new HashMap();
         parameter.put("LOGO", context.getRealPath("/WEB-INF/web-reports/logoues.gif"));
-        parameter.put("escuela",getCodigoEscuela());
-        JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, parameter,getConnection() );
-
+        //Saber de que reporte viene y que parametros le corresponden
+        switch (getNombreReporte()){
+            case "rServicioProfesional":
+                parameter.put("escuela", getCodigoEscuela());
+                break;
+            case "rptTGFinalizados":
+                parameter.put("id_escuela", Integer.parseInt(viewparams.get("id_escuela")));
+                parameter.put("anio", Integer.parseInt(getFilterAnio()));
+                parameter.put("estado", getFilterEstadoTGP());
+                break;
+            case "rptProyectosFinalizados":
+                parameter.put("id_escuela", Integer.parseInt(viewparams.get("id_escuela")));
+                parameter.put("anio", Integer.parseInt(getFilterAnio()));
+                parameter.put("estado", getFilterEstadoTGP());
+                break;
+        }
+        //Generar el reporte
+        JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, parameter, getConnection() );
         byte[] bytes = JasperExportManager.exportReportToPdf(jasperPrint);
             HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
             response.setContentType("application/pdf");
@@ -60,7 +86,33 @@ public class ReporteBean {
 			
         FacesContext.getCurrentInstance().responseComplete();
     }
-
+    
+    public void identifyReport(String reporte){
+        switch (reporte){
+            case "ServiciosProfesionales":
+                setNombreReporte("rServicioProfesional");
+                break;
+            case "TGFinalizados":
+                setNombreReporte("rptTGFinalizados");
+                break;
+            case "ProyectosFinalizados":
+                setNombreReporte("rptProyectosFinalizados");
+                break;
+        }
+    }
+    
+    //Para habilitar o dehabilitar el boton aceptar donde generara el reporte
+    public void onSelected() {
+        this.disabled = this.codigoEscuela.length()==1;
+    }
+    
+    public void validateChooseYear(){
+        if(this.filterAnio.length() == 1 || this.filterEstadoTGP.length() == 1)
+            disabled = true;
+        else
+            disabled = false;
+    }
+    
     /**
      * @return the codigoEscuela
      */
@@ -74,7 +126,61 @@ public class ReporteBean {
     public void setCodigoEscuela(String codigoEscuela) {
         this.codigoEscuela = codigoEscuela;
     }
+    
+    /**
+     * @return 
+     */
+    public String getNombreReporte() {
+        return nombreReporte;
+    }
+
+    /**
+     * @param 
+     */
+    public void setNombreReporte(String nombreReporte) {
+        this.nombreReporte = nombreReporte;
+    }
+    
+    /**
+     * @return the disabled
+     */
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    /**
+     * @param disabled the disabled to set
+     */
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+    
+    /**
+     * @return 
+     */
+    public String getFilterAnio() {
+        return filterAnio;
+    }
+
+    /**
+     * @param 
+     */
+    public void setFilterAnio(String filterAnio) {
+        this.filterAnio = filterAnio;
+    }
    
-   
+    /**
+     * @return 
+     */
+    public String getFilterEstadoTGP() {
+        return filterEstadoTGP;
+    }
+
+    /**
+     * @param 
+     */
+    public void setFilterEstadoTGP(String filterEstadoTGP) {
+        this.filterEstadoTGP = filterEstadoTGP;
+    }
     
 }
